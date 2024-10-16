4aae4ba422c9234a7e898ca37d1e1593e9779b19
Beka Chitadze <bchitadze@credo.ge>
2024-10-16 16:57:15 +0400

    davamate Excercise3

---
+++ src/test/java/Excercise3.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Excercise3 {
+    public static void main(String[] args) {
+
+        int[] integers = new int[10];
+        int input_value = 5;
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(input_value + " x " + i + " = " + (input_value * i));
+       }
+    }
+}
