f75d22220398aea5fc6cd34e48cebe9e8833f61f
Beka Chitadze <bchitadze@credo.ge>
2024-10-16 11:13:20 +0400

    davamata davaleba1

--- src/test/java/Exercise1.java
+++ src/test/java/Exercise1.java
@@ -9,7 +9,15 @@ public class Exercise1 {
 
         if (score < 0 || score > 100) {
             System.out.println("ჩაწერეთ ტიცხვი 0-დან 100-ჩათვლით");
-        } else if (score >= 90);
+        } else if (score >= 90) {
             System.out.println("A");
-        }
+        } else if (score >= 80) {
+            System.out.println("B");
+        } else if (score >= 70) {
+            System.out.println("C");
+        } else if (score >= 60) {
+            System.out.println("D");
+        } else
+            System.out.println("F");
+    }
 }
