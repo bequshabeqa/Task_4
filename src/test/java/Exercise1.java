03f2792b87b255f6c65b9f538686d04ef705dafc
Beka Chitadze <bchitadze@credo.ge>
2024-10-16 10:54:00 +0400

    Excercise_1

---
+++ src/test/java/Exercise1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Exercise1 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int score = scanner.nextInt();
+
+        if (score < 0 || score > 100) {
+            System.out.println("ჩაწერეთ ტიცხვი 0-დან 100-ჩათვლით");
+        } else if (score >= 90);
+            System.out.println("A");
+        }
+}
