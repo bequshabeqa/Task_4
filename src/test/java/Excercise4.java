dcd44ba035ab5f5a7ade11f1e25db0eaa8977012
Beka Chitadze <bchitadze@credo.ge>
2024-10-18 12:11:17 +0400

    davamate davaleba 4

--- src/test/java/Excercise4.java
+++ src/test/java/Excercise4.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class Excercise4 {
     public static void main(String[] args) {
+        System.out.println("შეიყვანეთ n-ის მნიშვნელობა");
+        Scanner scanner = new Scanner(System.in);
 
+        int n = scanner.nextInt();
+        int sum = 0;
+        int i = 2;
+
+        while (i <= n) {
+            sum += i;
+            i += 2;
+        }
+        System.out.println(" ჯამი = " + sum);
     }
 }
